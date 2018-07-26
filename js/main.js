758b46b25d441e23b0b3252579be155b82e3cdee
VladTheImplier <tt_cox@hotmail.com>
2018-07-27 01:15:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -152,19 +152,84 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("demo");
+var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(m) {
+  showSlides2(slideIndex2 += m);
+}
+
+function currentSlide2(m) {
+  showSlides2(slideIndex2 = m);
+}
+
+function showSlides2(m) {
+var j;
+var slides2 = document.getElementsByClassName("mySlides2");
+var dots2 = document.getElementsByClassName("demo2");
+var captionText2 = document.getElementById("caption2");
+    if (m > slides2.length) {slideIndex2 = 1}
+    if (m < 1) {slideIndex2 = slides2.length}
+    
+    for (j = 0; j < slides2.length; j++) {
+        slides2[j].style.display = "none";
+        
+    }
+    for (j = 0; j < dots2.length; j++) {
+        dots2[j].className = dots2[j].className.replace(" active", "");
+        
+    }
+    slides2[slideIndex2-1].style.display = "block";
+    dots2[slideIndex2-1].className += " active";
+    captionText2.innerHTML = dots2[slideIndex2-1].alt;
+}
+
+
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(k) {
+  showSlides3(slideIndex3 += k);
+}
+
+function currentSlide3(k) {
+  showSlides3(slideIndex3 = k);
+}
+
+function showSlides3(k) {
+var l;
+var slides3 = document.getElementsByClassName("mySlides3");
+var dots3 = document.getElementsByClassName("demo3");
+var captionText3 = document.getElementById("caption3");
+    if (k > slides3.length) {slideIndex3 = 1}
+    if (k < 1) {slideIndex3 = slides3.length}
+    
+    for (l = 0; l < slides3.length; l++) {
+        slides3[l].style.display = "none";
+        
+    }
+    for (l = 0; l < dots3.length; l++) {
+        dots3[l].className = dots3[l].className.replace(" active", "");
+        
+    }
+    slides3[slideIndex3-1].style.display = "block";
+    dots3[slideIndex3-1].className += " active";
+    captionText3.innerHTML = dots3[slideIndex3-1].alt;
 }
